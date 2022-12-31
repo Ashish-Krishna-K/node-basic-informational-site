d6e03393e89c2471164d1cf61d0348bdc51d31cd
Ashish-Krishna-K <ashishk.krishna@gmail.com>
2022-12-31 16:01:35 +0530

    Adds express to the project.

--- index.js
+++ index.js
@@ -1,32 +1,15 @@
-const http = require('http');
-const fs = require('fs');
+const express = require('express');
 
-const hostname = 'localhost';
+const app = express();
+
 const port = 8080;
 
-const readFile = (path) => {
-    try {
-        if (path === '/') {
-            return fs.readFileSync('index.html');
-        };
-        if (path === '/about') {
-            return fs.readFileSync('about.html');
-        };
-        if (path === '/contact-me') {
-            return fs.readFileSync('contact-me.html');
-        };
-        return fs.readFileSync('404.html');
-    } catch (error) {
-        console.error(error)
-    }
-}
+app.get('/', (req, res) => res.sendFile(`${__dirname}/index.html`));
 
-const server = http.createServer((req, res) => {
-    const path = req.url;
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end(readFile(path));
-});
+app.get('/about', (req, res) => res.sendFile(`${__dirname}/about.html`));
 
+app.get('/contact', (req, res) => res.sendFile(`${__dirname}/contact-me.html`));
 
-server.listen(port, hostname, () => console.log(`Server running at http://${hostname}:${port}/`));
+app.use((req, res, next) => res.status(404).sendFile(`${__dirname}/404.html`));
+
+app.listen(port, () => console.log(`Server running at http://localhost:${port}/`));
